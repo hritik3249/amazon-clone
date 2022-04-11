85198c2d627d2cf88fb235569f7e246ddccbd873
Hritik Ray <hritik3249@gmail.com>
2022-04-11 23:04:39 +0530

    Started working on Amazon Clone

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+import Header from "./Header";
+function App() {
+  return (
+    // BEM
+    <div className="App">
+      <Header />
+      {/* HOME */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAp2qQp38bjvILdflBRZVmi6ykRWZuf5MM",
+    authDomain: "challenge-83f7e.firebaseapp.com",
+    projectId: "challenge-83f7e",
+    storageBucket: "challenge-83f7e.appspot.com",
+    messagingSenderId: "397420403662",
+    appId: "1:397420403662:web:79ad0af1490948858fa99f",
+    measurementId: "G-LF41M1VEYC"
+  };
