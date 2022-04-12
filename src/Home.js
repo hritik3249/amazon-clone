b7bf2417910524c5ecb493118939500c3e3880b4
Hritik Ray <hritik3249@gmail.com>
2022-04-13 01:35:04 +0530

    Started working on amazon clone

--- src/Home.js
+++ src/Home.js
@@ -13,7 +13,7 @@ function Home() {
             </div>
             <div className="home_row">
                 <Product />
-                
+                <Product />
             </div>
             <div className="home_row">
 
